d9b8fb3c36cddbb8ef87535ccadb74b2694d059e
warmwhalefy <angelhfy1508@gmail.com>
2021-03-26 02:34:13 +0800

    SortCommand edit

--- src/main/java/seedu/hdbuy/data/exception/InvalidSortException.java
+++ src/main/java/seedu/hdbuy/data/exception/InvalidSortException.java
@@ -1,6 +1,6 @@
 package seedu.hdbuy.data.exception;
 
-public class InvalidSortException extends Exception{
+public class InvalidSortException extends Exception {
     public InvalidSortException() {
         super("is not a valid sort. Please enter either asc or desc");
     }
